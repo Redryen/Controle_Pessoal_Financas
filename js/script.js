f4ed17e9b1ef66513643ea696fb5f40c5d6302dd
Redryen <redryen.chula.ifpr.2023@gmail.com>
2025-12-02 21:32:58 -0300

    save

--- js/script.js
+++ js/script.js
@@ -1,38 +1,9 @@
-document.getElementById("formsalario").addEventListener("submit", function(event){
-      event.preventDefault();
-
-      var salario = document.getElementById("salario").value
-
-      var salarios = {salario:salario}
-
-      var lista_salarios = JSON.parse(localStorage.getItem('listagem'))|| []
-
-      lista_salarios.push(despesa)
-    
-    localStorage.setItem('listagem', JSON.stringify(lista_salarios))
-    document.getElementById('formsalario').reset()
-    exibir_salarios()
-})
-
-function exibir_salarios(){
-    var lista_salarios= JSON.parse(localStorage.getItem('listagem')) || []
-    var output = document.getElementById('output')
-    output.innerHTML = ''
-    for(let i=0; i<lista_salarios.length; i++){
-        let li = document.createElement('li')
-        li.textContent = 'Salario:' +lista_salarios[i].salario + 
-        output.appendChild(li)
-    }
-}
-
-
-
-
 
 document.getElementById("formdespesa").addEventListener("submit", function(event){
 
     event.preventDefault();
 
+    var salario = document.getElementById("salario").value
     var data = document.getElementById("data").value
     var nome = document.getElementById("nome").value
     var valor = document.getElementById("valor").value
@@ -46,6 +17,8 @@ document.getElementById("formdespesa").addEventListener("submit", function(event
     localStorage.setItem('listagem', JSON.stringify(lista_despesas))
     document.getElementById('formdespesa').reset()
     exibir_despesas()
+    exibir_resumo()
+
 })
 
 
@@ -55,7 +28,17 @@ function exibir_despesas(){
     output.innerHTML = ''
     for(let i=0; i<lista_despesas.length; i++){
         let li = document.createElement('li')
-        li.textContent = 'Data:' +lista_despesas[i].data + '      |    Nome:' +lista_despesas[i].nome + '    |  valor:' +lista_despesas[i].valor
+        li.textContent = '      |Data:' +lista_despesas[i].data + '      |    Nome:' +lista_despesas[i].nome + '    |  valor:' +lista_despesas[i].valor
         output.appendChild(li)
     }
 }
+
+function exibir_resumo(){
+    var lista_despesas = JSON.parse(localStorage.getItem('listagem')) || []
+    var resumo = document.getElementById('resumo')
+    resumo.innerHTML = ''
+    for(let i=0; i<lista_despesas.length; i++){
+        let li = document.createElement('li')
+        li.textContent = 'Salário:' +lista_despesas[i].salario + '      |Total despesas:' +lista_despesas[i].tdespesas + '      |    Saldo final:' +lista_despesas[i].saldo + '    |  valor:' +lista_despesas[i].valor
+        resumo.appendChild(li)
+    } }
